587316950d768955e285dc6904adcdf1b4df1a75
ToniAnton22 <frincuanton@yahoo.com>
2023-04-24 22:00:36 +0300

    added the some more features

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,8 @@ import { InferGetServerSidePropsType } from 'next'
 
 export async function getServerSideProps(context) {
   try {
-    await clientPromise
+    const client =await clientPromise
+    const db = client.db("New_forest-escape")
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
