d1516e1ebdf244b9d9939cc371bc4d20f6a591df
ToniAnton22 <frincuanton@yahoo.com>
2023-05-10 02:34:41 +0300

    changed route

--- pages/houses.jsx
+++ pages/houses.jsx
@@ -25,7 +25,7 @@ const orders = () => {
                     {data.map((order,id)=>{
                         return (
                           <li key={id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
-                          <a href={`/houses/123`} className='flex'>
+                          <a href={`/house/123`} className='flex'>
                             <div className='pl-4'>
                               <p className='text-gray-800 font-bold'>{order.name.first} {order.name.last}</p>
                               <p className='text-gray-800 text-sm'>{order.house}</p>
